7c66af4e2707b7000adf296d9cc5d035ce46929c
DevPCW <rorndksk123@naver.com>
2023-03-21 22:10:47 +0900

    2023-03-21_changwoo: index html / CMRespDto / IndexController

---
+++ src/main/java/com/study/devpcw/web/dto/CMRespDto.java
@@ -0,0 +1,17 @@
+package com.study.devpcw.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CMRespDto <T> {
+
+    private int code;
+
+    private String message;
+
+    private T data;
+}
