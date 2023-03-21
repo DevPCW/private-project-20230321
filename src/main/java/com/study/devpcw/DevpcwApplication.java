11c22ce4824bd6367898d73d7e81e5728d2cebc0
DevPCW <rorndksk123@naver.com>
2023-03-21 19:12:58 +0900

    2023-03-21_changwoo: 개인 프로젝트 시작

---
+++ src/main/java/com/study/devpcw/DevpcwApplication.java
@@ -0,0 +1,13 @@
+package com.study.devpcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevpcwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevpcwApplication.class, args);
+	}
+
+}
