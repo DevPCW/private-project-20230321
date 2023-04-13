6f1d0c3ab0cb3c706f410cffd81562177b7a2c53
DevPCW <rorndksk123@naver.com>
2023-04-13 09:56:37 +0900

    2023-04-13_changwoo: index.js 수정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,37 @@
 window.onload = () => {
     IndexButtonService.getInstance().loginButtonOnclickEvent();
+    IndexPrincipalEventService.getInstance().loginSuccessHeaderEvent();
+}
+
+class PrincipalApi {
+    static #instance = null;
+    static getInstance() {
+        if(this.#instance == null) {
+            this.#instance = new PrincipalApi();
+        }
+
+        return this.#instance;
+    }
+
+    getPrincipal() {
+        let responseData = null;
+
+        $.ajax({
+            async: false,
+            type: 'get',
+            url: "/api/account/principal",
+            dataType: "json",
+            success: response => {
+                responseData = response.data;
+                console.log(responseData);
+            },
+            error: error => {
+                console.log(error);
+            }
+        });
+
+        return responseData;
+    }
 }
 
 class IndexButtonService {
@@ -17,5 +49,59 @@ class IndexButtonService {
         infoLoginButton.onclick = () => {
             location.href = '/account/login';
         }
+    }
+}
+
+class IndexPrincipalEventService {
+    static #instance = null;
+    static getInstance() {
+        if(this.#instance == null) {
+            this.#instance = new IndexPrincipalEventService();
+        }
+        return this.#instance;
+    }
+
+    loginSuccessHeaderEvent() {
+        const infoLoginNav = document.querySelectorAll('.info-container-nav')[0];
+        const infoRegisterNav = document.querySelectorAll('.info-container-nav')[1];
+        const principalData = PrincipalApi.getInstance().getPrincipal();
+
+        if(principalData.userId !== null) {
+            infoLoginNav.innerHTML = `
+                <li><a href="javascript:void(0);" class="info-container-nav">${principalData.user.name}님</a></li>
+            `;
+
+            infoLoginNav.onclick = () => {
+                location.reload();
+            }
+
+            infoRegisterNav.innerHTML = `
+                <li><a href="javascript:void(0);" class="info-container-nav">로그아웃</a></li>
+            `;
+
+            infoRegisterNav.onclick = () => {
+                // 사용자 정보 제거
+                $.ajax({
+                    type: 'POST',
+                    url: '/logout',
+                    success: () => {
+                        // 로그아웃 성공시 로그인 페이지로 이동
+                        location.replace("/account/login");
+                    },
+                    error: error => {
+                        console.log(error);
+                    }
+                });
+            }
+        } else if(principalData.userId == null) {
+            infoLoginNav.innerHTML = `
+                <li><a href="javascript:void(0);" class="info-container-nav">로그인</a></li>
+            `;
+
+            infoRegisterNav.innerHTML = `
+                <li><a href="javascript:void(0);" class="info-container-nav">회원가입</a></li>
+            `;
+        }
+
     }
 }
